81be749f86ccf5718f13e47d0f6890068d74a0d4
Fayas Muhammed <muhammedfayas796@gmail.com>
2024-08-06 14:58:57 +0530

    test commit

--- web/models.py
+++ web/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-
+from django.db.models.signals import post_save
 
 
 
@@ -21,8 +21,19 @@ class UserProfile_model(models.Model):
     
     profile_picture=models.ImageField(upload_to='images')
 
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
     created_date=models.DateField(auto_now_add=True)
 
     updated_date=models.DateField(auto_now=True)
 
     is_active=models.BooleanField(default=True)
+
+
+def create_profile(sender,instance,created,**kwargs):
+
+    if created:
+
+        UserProfile_model.objects.create(user=instance)
+
+post_save.connect(sender=User,receiver=create_profile)
